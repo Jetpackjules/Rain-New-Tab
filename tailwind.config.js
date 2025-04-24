e343b5ba1b6c50dc378038a0c762a466ed985740
JR <46934175+Jetpackjules@users.noreply.github.com>
2025-04-24 12:13:41 -0700

    Removed public link from index3.html (removed entirely)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './rainyExtension/index.html'],
+  content: [
+        // Add paths to all template files using Tailwind classes
+        './rainyExtension/index.html',
+        // './rainyExtension/index2.html', // If used
+        // './rainyExtension/index3.html', // If used
+        './src/*.js',               // Include your source JS files
+        './src/*.jsx',              // Include JSX if your React components use it
+      ],
+      
+      // './src/**/*.{js,jsx,ts,tsx}', './rainyExtension/index.html'],
   theme: {
     extend: {},
   },
